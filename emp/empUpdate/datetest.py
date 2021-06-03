ed1224048dbb618ac1a8b5c8c1f5dd41be4f8c31
wangguosheng <1404429103@qq.com>
2021-06-03 22:38:24 +0800

    add

--- emp/empUpdate/datetest.py
+++ emp/empUpdate/datetest.py
@@ -1,13 +1,33 @@
 import datetime
+import MySQLdb
 from chinese_calendar import is_workday
 
 def datetest():
-    date = datetime.datetime.now().date()
-    #date = datetime.datetime (2020, 8, 9)
-    if is_workday(date):
-        print("工作日")
+    date1 = datetime.datetime.now().date()
+    #date1 = datetime.datetime (2020, 8, 9)
+    date = datetime.datetime.now().weekday() +1
+    print(date)
+    # if is_workday(date1):
+    #     print("工作日")
+    # else:
+    #     print("非工作日")
+    if date == 4:
+        print("今天星期四")
     else:
-        print("非工作日")
+        print("今天不是星期四")
+
+def mytest():
+    sql = ("CALL emp_update_dept();")
+    sql2 =("CALL emp_update_dept_h();")
+    print(sql)
+    if sql == "":
+        print("111111")
+        if sql2 =="":
+            print("333333")
+        else:
+            print("44444")
+    else:
+        print("22222")
 
 
-datetest()
+mytest()
---
+++ emp/empUpdate/testEmpDeptUpdate.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+from chinese_calendar import is_workday
+import MySQLdb
+
+host = "114.115.161.26"
+user = "ctgehr_dev"
+passwd = "dj^kj3k672DA"
+port = 3306
+db = "ctgehr"
+
+def mytask():
+        # 服务器地址 ,端口， 登录账号 , 密码 ,数据库名称
+        conn = MySQLdb.connect(host=host,port=port,user=user,passwd=passwd,db=db,charset='utf8',)
+        cursor = conn.cursor()
+        #查询今天是否有变更部门的员工
+        date1 = datetime.datetime.now().weekday() + 1
+        #判断今天是否为星期一
+        if str(date1) == 4:
+                sql = ("CALL emp_update_dept();CALL emp_update_dept_h();")
+                cursor.execute (sql)
+                print ("=======================================================================================")
+                print("今天是星期" + date1 + "\n" +sql+ "执行成功")
+                # 对fetchall获取到的结果SQL结果集做换行显示
+                all = '\n'.join (str (e) for e in cursor.fetchall ())
+                if all == '':
+                        print ("无变更部门的员工")
+                        print ("=======================================================================================")
+                else:
+                        print ("变更部门的员工")
+                        print (all)
+                        print ("=======================================================================================")
+                        # print('\n'.join(str(e) for e in cursor.fetchall()))
+                cursor.nextset()
+                allNext = '\n'.join (str (e) for e in cursor.fetchall ())
+                if allNext =="":
+                        print("11111")
+                else:
+                        print("22222")
+        else:
+                sql = ("CALL emp_update_dept();")
+                cursor.execute (sql)
+                print ("=======================================================================================")
+                print("今天是星期" + str(date1) + "\n" + sql + "执行成功")
+                # 对fetchall获取到的结果SQL结果集做换行显示
+                all = '\n'.join (str (e) for e in cursor.fetchall ( ))
+                if all == '':
+                        print ("无变更部门的员工")
+                        print ("=======================================================================================")
+                else:
+                        print ("变更部门的员工")
+                        print (all)
+                        print ("=======================================================================================")
+        # 查询今天是否有新增的部门
+        # 判断当天日期是否是工作日
+        date = datetime.datetime.now().date()
+        if is_workday(date):
+                #如果是工作日的逻辑
+                sql1 = ("CALL dept_add();")
+                cursor.execute (sql1)
+                print (sql1 + "执行成功")
+                all1 = '\n'.join (str (e) for e in cursor.fetchall ( ))
+                if all1 == '':
+                        print ("无新增的部门")
+                        print ("=======================================================================================")
+                else:
+                        print ("当天新增部门")
+                        print (all1)
+                        print ("=======================================================================================")
+        else:
+                #非工作日逻辑
+                # sql1 = ("CALL dept_add();")
+                # cursor.execute (sql1)
+                # print (sql1 + "执行成功")
+                # all1 = '\n'.join (str (e) for e in cursor.fetchall ( ))
+                print("非工作日")
+        cursor.close()
+        conn.close()
+mytask()
+
+
+
+# class SelectMySQL(object):
+#     def select_data(self, sql):
+#         result = []
+#         try:
+#             conn = MySQLdb.connect(host=host,
+#                                    port=port,
+#                                    user=user,
+#                                    passwd=passwd,
+#                                    db=db,
+#                                    charset='utf8', )
+#             cur = conn.cursor()
+#             cur.execute(sql)
+#             alldata = cur.fetchall()
+#             print(alldata \n)
+#             for rec in alldata:
+#                 result.append(rec[5])  # 注意，我这里只是把查询出来的第一列数据保存到结果中了,如果是多列的话，稍微修改下就ok了
+#         except Exception as e:
+#             print('Error msg: ' + e)
+#         finally:
+#             cur.close()
+#             conn.close()
+#
+#         return result
+#
+#     def get_result(self, sql, filename):
+#         print(sql)
+#         results = self.select_data(sql)
+#         print('The amount of datas: %d' % (len(results)))
+#         with open(filename, 'w') as f:
+#             for result in results:
+#                 f.write(str(result) + '\n')
+#         print('Data write is over!')
+#         return results
+#
+#
+# if __name__ == '__main__':
+#     sql = "CALL emp_update_dept();"
+#     select = SelectMySQL()
+#     result1 = select.get_result(sql, 'namemsg.txt')
+#     print(result1)
+
